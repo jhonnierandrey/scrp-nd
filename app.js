64838b2304c08b3ceb5067ea0c3e775436bd7500
jhonnierandrey <jhonnierandrey@outlook.com>
2021-09-28 20:31:35 -0300

    moving server from app.js

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const path = require('path');
 
-const PORT = 3000
-
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -42,4 +40,20 @@ app.post('/api', (req, res) => {
         }).catch( err => console.log(err));
 })
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+  
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+});
+  
+module.exports = app;
